778fc57703e67530bf632f700f1b686bef7dd266
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-10-16 22:21:46 -0400

    returns

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ var sayBye = function(){
 }
 
 sayBye();
+
+
+function sing(){
+    console.log("AHHHHHH");
+    console.log("TEEEEEEE");
+}
+
+sing();
+
+
+
+function sing2(song){
+    console.log(song);
+}
+
+sing2("la de dah");
+
+
+function multiply(a, b) {
+    if(a > 10 || b > 10) {
+        return "That's too hard";
+    }else{
+        return a*b;
+    }
+}
