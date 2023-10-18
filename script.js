3ac1bd71c01de379b9273dcf83d9177dd65e89e4
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-10-17 23:15:14 -0400

    completed loops

--- script.js
+++ script.js
@@ -16,8 +16,32 @@ var todos = [
 //     counterOne--;
 // }
 
-var counterTwo = 10
-do {
-    console.log(counterTwo);
-    counterTwo--;
-} while (counterTwo > 0);
+// var counterTwo = 10
+// do {
+//     console.log(counterTwo);
+//     counterTwo--;
+// } while (counterTwo > 0);
+
+var todosLength = todos.length;
+for (var i = 0; i < todosLength; i++) {
+    console.log(i);
+}
+
+// todos.forEach(function(todo, i) {
+//     console.log(todo, i);
+// })
+
+var importantTodos = [
+    "clean room!", 
+    "brush teeth!",
+    "excercise!", 
+    "study javascript!", 
+    "eat healthy!"
+];
+
+function logTodos(todo, i) {
+    console.log(todo, i);
+}
+
+todos.forEach(logTodos);
+importantTodos.forEach(logTodos);
