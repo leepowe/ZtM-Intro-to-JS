a956a7f4a5a4f0eda7eb8e1a84395424fcaaba21
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-11-06 13:27:35 -0500

    ES7 updates

---
+++ 169. ES7/script.js
@@ -0,0 +1,14 @@
+//Includes
+console.log('Helloooo'.includes('o')); //true
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('dog')); //true
+console.log(pets.includes('bird')); //false
+
+const square = (x) => x ** 2;
+
+console.log(square(2));
+
+const cube = (x) => x ** 3;
+
+console.log(cube(3));
