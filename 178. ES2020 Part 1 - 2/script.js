1c5ad5921d1fffbb417a9d111eb607a8dd161a93
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-11-06 22:59:48 -0500

    ES2020 part 2

--- 178. ES2020 Part 1 - 2/script.js
+++ 178. ES2020 Part 1 - 2/script.js
@@ -4,7 +4,7 @@ console.log(Number.MAX_SAFE_INTEGER + 10); //9007199254741000
 console.log(9007199254741000 - 1); //9007199254741000 number did not change
 console.log(9007199254741000n - 1n); //9007199254740999n
 
-//Optional Chaining Operator
+//Optional Chaining Operator "?."
 let will_pokemon = {
     pikachu: {
         species: 'Mouse Pokemon', 
@@ -21,8 +21,18 @@ let andrei_pokemon = {
     }
 }
 
-let weight = will_pokemon.pikachu.weight
-console.log('weight:', weight);
+// let weight = will_pokemon.pikachu.weight
+// console.log('weight:', weight);
+if (andrei_pokemon.pikachu && andrei_pokemon.pikachu.weight) {
+    let weight2 = andrei_pokemon.pikachu.weight;
+} else {
+    let weight2 = undefined;
+}
 
-let weight2 = andrei_pokemon.raichu.weight
-console.log('weight:', weight);
+
+
+// let weight2 = andrei_pokemon.raichu.weight
+// console.log('weight:', weight);
+
+let weight3 = andrei_pokemon?.pikachu?.weight
+console.log(weight3);
