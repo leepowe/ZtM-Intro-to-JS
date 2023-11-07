a7b76c345ce1de7fbee916da4ec57a47dbc224f6
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-11-06 23:17:27 -0500

    ES2020 part 3

--- 178. ES2020 Part 1 - 3/script.js
+++ 178. ES2020 Part 1 - 3/script.js
@@ -4,7 +4,7 @@ console.log(Number.MAX_SAFE_INTEGER + 10); //9007199254741000
 console.log(9007199254741000 - 1); //9007199254741000 number did not change
 console.log(9007199254741000n - 1n); //9007199254740999n
 
-//Optional Chaining Operator "?."
+//Optional Chaining Operator "?." (and chaining)
 let will_pokemon = {
     pikachu: {
         species: 'Mouse Pokemon', 
@@ -17,7 +17,8 @@ let andrei_pokemon = {
     raichu: {
         species: 'Mouse Pokemon', 
         height: 0.8, 
-        weight: 30
+        weight: 30, 
+        power: 'lightning'
     }
 }
 
@@ -36,3 +37,9 @@ if (andrei_pokemon.pikachu && andrei_pokemon.pikachu.weight) {
 
 let weight3 = andrei_pokemon?.pikachu?.weight
 console.log(weight3);
+
+//Nullish Coalescing Operator "??" (or chaining) checks if null or undefined
+let power = andrei_pokemon?.raichu?.power || 'no power'
+console.log('or', power);
+let power2 = andrei_pokemon?.raichu?.power ?? 'no power'
+console.log('nullish', power2)
