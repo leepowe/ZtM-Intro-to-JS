dba7f23b603f8cdc9eab9344a88152a0b8f86e64
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-11-06 22:51:10 -0500

    ES2020 part 1

---
+++ 178. ES2020 Part 1/script.js
@@ -0,0 +1,28 @@
+//BigInt
+console.log(Number.MAX_SAFE_INTEGER + 1); //9007199254740992
+console.log(Number.MAX_SAFE_INTEGER + 10); //9007199254741000
+console.log(9007199254741000 - 1); //9007199254741000 number did not change
+console.log(9007199254741000n - 1n); //9007199254740999n
+
+//Optional Chaining Operator
+let will_pokemon = {
+    pikachu: {
+        species: 'Mouse Pokemon', 
+        height: 0.4, 
+        weight: 6
+    }
+}
+
+let andrei_pokemon = {
+    raichu: {
+        species: 'Mouse Pokemon', 
+        height: 0.8, 
+        weight: 30
+    }
+}
+
+let weight = will_pokemon.pikachu.weight
+console.log('weight:', weight);
+
+let weight2 = andrei_pokemon.raichu.weight
+console.log('weight:', weight);
