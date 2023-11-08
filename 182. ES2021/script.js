7e781b0651430cee4ed56966244bc35ffff4f1f6
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-11-07 22:39:48 -0500

    ES2021

---
+++ 182. ES2021/script.js
@@ -0,0 +1,7 @@
+const str = 'ztm is the best of the best';
+console.log(str);
+// ztm is the best of the best
+
+const str2 = str.replaceAll('best', 'worst');
+console.log(str2);
+//ztm is the worst of the worst
